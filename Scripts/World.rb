578cf1056a93ac95c2485e419217362785830205
datalurkur <datalurkur@gmail.com>
2010-02-10 15:14:12 -0600

    Makes worldgen a factory, adds the World class

---
+++ Scripts/World.rb
@@ -0,0 +1,21 @@
+#!../ruby/bin/ruby
+
+require 'MountainHomeDSL.rb'
+
+class World
+	def initialize(params={})
+		@height = params[:height] || 33
+		@width  = params[:width]  || 33
+		@depth  = params[:depth]  || 17
+
+		@tiles  = Array.new(@width) { Array.new(@height) { Array.new(@depth) } }
+	end
+
+	def setTile(x,y,z,tile)
+		@tiles[x][y][z] = tile
+	end
+
+	def getTile(x,y,z)
+		@tiles[x][y][z]
+	end
+end
