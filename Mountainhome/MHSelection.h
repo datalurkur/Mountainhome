ffc0cbe96cd69f33418b260d06d56cc7697c732c
datalurkur <datalurkur@gmail.com>
2010-03-15 23:00:33 -0500

    Small update to MHSelection, postponing future development pending discussion.

--- Mountainhome/MHSelection.h
+++ Mountainhome/MHSelection.h
@@ -1,14 +1,10 @@
 #ifndef _MHSELECTION_H_
 #define _MHSELECTION_H_
 
-#include <Render/Entity.h>
-
-class Entity;
-
 class MHSelection {
 public:
-    MHSelection() {}
-    virtual ~MHSelection() {}
+    MHSelection();
+    virtual ~MHSelection();
 
     //void initialize(std::list <MHTypeObject*> &selected) {}
 
