96410250e56a5a1bda3a1f684d97d030e972aafe
datalurkur <datalurkur@gmail.com>
2010-04-19 14:58:33 -0500

    Adds a missing file, derp.

---
+++ Mountainhome/LiquidPool.cpp
@@ -0,0 +1,11 @@
+#include "LiquidPool.h"
+
+LiquidPool::LiquidPool(const Vector3 &dimensions): TileGroup<Vector2>(Vector3(0.0), dimensions, Vector2(0.0), NULL) {
+}
+
+LiquidPool::~LiquidPool() {
+}
+
+float LiquidPool::getVolume() {
+    
+}
