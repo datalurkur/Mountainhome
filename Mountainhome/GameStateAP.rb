1a34ebcf9d157458bcf90e054ad7a99fca957fd9
Zphobic <paulwuersig@gmail.com>
2011-04-11 22:45:06 -0500

    Minor cleanup.

--- Mountainhome/GameStateAP.rb
+++ Mountainhome/GameStateAP.rb
@@ -6,16 +6,18 @@ class GameStateAP
 
     def self.events
         [
-        #:name,             :key or event
-        #:key, or [:key,:mod]  :on_press, :on_release
-        [:KEY_BACKQUOTE, :toggle_console],
-        [:KEY_TAB, :toggle_mouselook],
-        # Toggle between wireframe and filled when spacebar is pressed.
-        [:KEY_SPACE,    :toggle_filled],
+        #:key or [:key,:mod],  :on_press, :on_release
 
         # Toggle various debugging tools
-        [:KEY_f, :toggle_frustum_culling],
-        [:KEY_b,  :toggle_bounding_boxes],
+        [:KEY_f,  :toggle_frustum_culling],
+        [:KEY_b,   :toggle_bounding_boxes],
+        # Toggle mouse-look or mouse.
+        [:KEY_TAB,      :toggle_mouselook],
+        # Toggle the console.
+        [:KEY_BACKQUOTE,  :toggle_console],
+        # Toggle between wireframe and filled when spacebar is pressed.
+        [:KEY_SPACE,       :toggle_filled],
+
 
         # Return to MenuState
         [:KEY_q, :quit_to_menu],
@@ -35,13 +37,6 @@ class GameStateAP
         [:KEY_d,       :move_right,    :stop_right],
         [[:KEY_w, :MOD_SHIFT], :move_up,   :stop_up  ],
         [[:KEY_s, :MOD_SHIFT], :move_down, :stop_down],
-
-#        [KeyReleased.new(:KEY_d), :stop_right],
-#        [:stop_left,        KeyReleased.new(:KEY_a)],
-#        [:stop_forward,     KeyReleased.new(:KEY_w)],
-#        [:stop_backward,    KeyReleased.new(:KEY_s)],
-#        [:stop_up,          KeyReleased.new(:KEY_w, :MOD_SHIFT)],
-#        [:stop_down,        KeyReleased.new(:KEY_s, :MOD_SHIFT)],
         ]
     end
 end
