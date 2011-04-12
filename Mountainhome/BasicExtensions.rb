cd128fd6d6f17f7991c69d036008b988b09fb4f4
Zphobic <paulwuersig@gmail.com>
2011-04-11 22:00:20 -0500

    Moved the base class extensions into their own file.

---
+++ Mountainhome/BasicExtensions.rb
@@ -0,0 +1,46 @@
+#############################
+# Built in class extensions #
+#############################
+
+# Zip together and add, with size checking.
+class Array
+    def piecewise_add(array)
+        if array.size != self.size
+            raise ArgumentError, "Array sizes #{[array.size,self.size].inspect} don't match."
+        else
+            self.zip(array).map { |i,j| i+j }
+        end
+    end
+end
+
+class Symbol
+    def constantize () self.to_s.constantize end
+    def camelize    () self.to_s.camelize    end
+end
+
+class String
+    # Called on "String" or "Dwarf", returns the class types String or Dwarf
+    def constantize
+        name = self.camelize
+        Object.const_defined?(name) ?
+            Object.const_get(name) :
+            Object.const_missing(name)
+    end
+
+    # called on "foo/bar" yields Foo::Bar, or "dwarf_module" yields DwarfModule
+    def camelize
+        self.gsub(/\/(.?)/) { "::#{$1.upcase}" }.gsub(/(?:^|_)(.)/) { $1.upcase }
+    end
+end
+
+class Object
+    def self.delegate_to(obj, *methods)
+        methods.each do |method|
+            class_eval %{
+                def #{method}(*args)
+                    @#{obj}.#{method}(*args)
+                end
+            }
+        end
+    end
+end
---
+++ Mountainhome/RingBuffer.rb
@@ -0,0 +1,60 @@
+# Maintains a contiguous buffer within a larger array. Both sides of the buffer
+# can be added to and removed from in constant time, making it useful for things
+# like scrollback.
+class RingBuffer
+    attr_reader :size
+
+    def initialize(max_size)
+        @ary = Array.new(max_size)
+        @start = 0
+        @size = 0
+    end
+
+    def [](index); @size > 0 ? @ary[array_index(index)] : nil; end
+    def []=(index, value); @ary[array_index(index)] = value; end
+
+    def push(item);
+        if @size == @ary.size
+            @ary[@start] = item
+            self.start_forward
+        else
+            # Avoid array_index, as we specifically don't want to mod @size by @size.
+            @ary[(@start + @size) % @ary.size] = item
+            @size += 1
+        end
+
+        item
+    end
+
+    def pop()
+        if @size > 0
+            @size -= 1
+            @ary[@start + @size]
+        end
+    end
+
+    def unshift(item)
+        @size += 1 unless @size == @ary.size
+        self.start_backward
+        @ary[@start] = item
+    end
+
+    def shift()
+        if @size > 0
+            @size -= 1
+            self.start_forward
+            @ary[@start - 1]
+        end
+    end
+
+    def inspect
+        "[start: #{@start} size: #{@size} buffer: #{@ary.inspect}]"
+    end
+
+protected
+    def start_forward;  @start = (@start + 1) % @ary.size; end
+    def start_backward; @start = (@start - 1) % @ary.size; end
+    def array_index(index)
+        @size == 0 ? 0 : (@start + (index % @size)) % @ary.size
+    end
+end
