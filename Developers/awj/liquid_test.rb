f3c26a724f79f558e0e0b40bee8f0742ef69379b
datalurkur <datalurkur@gmail.com>
2011-06-06 17:36:06 -0500

    Improvement to the liquid flow algorithm

--- Developers/awj/liquid_test.rb
+++ Developers/awj/liquid_test.rb
@@ -1,12 +1,14 @@
+@max_size     = 100.0
 @liquid_grid = [
-    [0.0, 0.0, 0.0, 5.0],
+    [0.0, 0.0, 0.0, @max_size / 2.0],
     [0.0, 0.0, 0.0, 0.0],
-    [0.0, 0.0, 0.0, 3.0],
+    [0.0, 0.0, 0.0, @max_size / 5.0],
     [0.0, 0.0, 0.0, 0.0]
 ]
+@liquid_speed = 1.5
+@threshold    = @max_size / 10000.0
 
 @dirty_nodes = [[0,3],[2,3]]
-@next_dirty_nodes = []
 
 def get(coords)
     @liquid_grid[coords[0]][coords[1]]
@@ -17,63 +19,57 @@ def set(coords, level)
 end
 
 def step
+    affected_nodes = @dirty_nodes.dup
     @dirty_nodes.each do |node|
-        flow_into = []
-        to_dirty  = []
+        this_height = get(node)
 
-        this_level = get(node)
-        (-1..1).each do |x_p|
-            (-1..1).each do |y_p|
-                next if x_p == 0 && y_p == 0
-                local = [node[0] + x_p, node[1] + y_p]
-                next if oob?(local)
-                local_level = get(local)
-                if local_level < this_level
-                    flow_into << local
-                end
-                to_dirty << local
-            end
-        end
-        next if flow_into.size == 0
-
-        @next_dirty_nodes.concat(to_dirty).uniq!
-
-        sorted_heights = flow_into.collect { |x| get(x) }.uniq.sort
-
-        sorted_heights.each do |target_height|
-            available = this_level - target_height
-            puts "Normalizing neighbors to height #{target_height} with #{available} volume available"
-            filtered = flow_into.select { |x| get(x) < target_height }
-            required = filtered.inject(0) { |sum,x| sum + (target_height - get(x)) }
-            if required > available
-                each_contribution = available / filtered.size
-                filtered.each { |x| set(x, get(x) + each_contribution) }
-                set(node, get(node) - available)
-
-                print_state
-                break
-            else
-                filtered.each { |x| set(x, target_height) }
-                set(node, get(node) - required)
-
-                print_state
+        lower_neighbors = []
+        (-1..1).collect do |x|
+            (-1..1).collect do |y|
+                next if x == 0 && y == 0
+                local_coords = [node[0] + x, node[1] + y]
+                next if oob?(local_coords)
+                local_height = get(local_coords)
+                (lower_neighbors << local_coords) if ((this_height - local_height) > @threshold)
             end
         end
 
-        max = flow_into.collect { |x| get(x) }.max
-        if get(node) != max
-            avg = (get(node) + (max * flow_into.size)) / (flow_into.size + 1)
-            ind_contr = avg - max
-            contribution = ind_contr * flow_into.size
-            flow_into.each do |x|
-                set(x, get(x) + ind_contr)
+        affected_nodes.concat(lower_neighbors)
+
+        # We shoot for the average of this node with its surrounds
+        target_height = ((lower_neighbors.inject(0) { |sum, local| sum + get(local) } + this_height) / (lower_neighbors.size + 1))
+
+        lower_neighbors.each do |neighbor|
+            delta = target_height - get(neighbor)
+            if delta > @liquid_speed
+                delta = @liquid_speed
             end
-            set(node, get(node) - contribution)
+
+            set(node, get(node) - delta)
+            set(neighbor, get(neighbor) + delta)
         end
     end
 
-    @dirty_nodes = @next_dirty_nodes
-    @next_dirty_nodes = []
+    print_grid
+
+    @dirty_nodes = []
+    affected_nodes.uniq.each do |node|
+        this_level = get(node)
+        (-1..1).each do |x|
+            (-1..1).each do |y|
+                next if x==0 && y==0
+                local = [node[0]+x,node[1]+y]
+                next if oob?(local)
+                local_level = get(local)
+                if (this_level - local_level) > @threshold
+                    @dirty_nodes << node
+                elsif (local_level - this_level) > @threshold
+                    @dirty_nodes << local
+                end
+            end
+        end
+    end
+    @dirty_nodes.uniq!
 end
 
 def oob?(coords)
@@ -88,7 +84,21 @@ def print_state
     puts "Dirty nodes: #{@dirty_nodes.inspect}"
 end
 
-10.times do |x|
+def print_grid
+    puts "INT GRID:"
+    @liquid_grid.each do |x|
+        puts "\t#{x.collect { |y| y.to_i }}"
+    end
+end
+
+puts "STARTING GRID:"
+print_grid
+
+1000.times do |x|
+    break if @dirty_nodes.empty?
+    puts "\n" + "=" * 50
     puts "Iteration #{x}"
     step
 end
+
+print_grid
